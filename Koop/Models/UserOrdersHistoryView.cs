136ef3134906bb42f81df1eca143f6bc7a9e5daf
Ewa Zelenska <ewa.zelenska@gmail.com>
2021-03-29 00:46:01 +0200

    3 views done: - Supplier Info - Basket - Name - User Order History

---
+++ Koop/Models/UserOrdersHistoryView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Koop.models
+{
+    public partial class UserOrdersHistoryView
+    {
+        public Guid? Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Guid? OrderId { get; set; }
+        public DateTime? OrderStopDate { get; set; }
+        public string Price { get; set; }
+        public string OrderStatusName { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
